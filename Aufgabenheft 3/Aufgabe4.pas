96ce3876435de6f5fdd76d769ac4fc5c358d9a0a
eberlful <markusbenedikteberl@googlemail.com>
2017-11-06 13:27:47 +0100

    ...

--- Aufgabenheft 3/Aufgabe4.pas
+++ Aufgabenheft 3/Aufgabe4.pas
@@ -28,18 +28,117 @@ Keinesfalls sollen die Werte der Knoten kopiert oder überschrieben werden.}
   procedure SortiereListe (var ioRefListe : tRefListe);
   { sortiert eine lineare Liste aufsteigend }
 
+  var
+  ZwischenSpeicher,
+  SourceList,
+  DestinationList,
+  DestAktList,
+  PreDestList : tRefListe;
+  i : integer;
 
+    procedure GibListeAus1(inListe1 : tRefListe);
+    { Gibt die Elemente von inListe aus }
+    var Zeiger1 : tRefListe;
+    begin
+      Zeiger1 := inListe1;
+      while Zeiger1 <> nil do
+        begin
+          writeln(Zeiger1^.info);
+          Zeiger1 := Zeiger1^.next;
+        end; { while }
+    end; { GibListeAus }
 
+  begin
+    SourceList := ioRefListe;
+    DestAktList := ioRefListe;
+    new (DestinationList);
+    i := 0;
+    writeln('Starte sortieren...');
+    writeln('Sourcelist^.info: ',ioRefListe^.info);
+    while SourceList <> nil do
+      begin
+        i := i + 1;
+        ZwischenSpeicher := SourceList;
+        SourceList := ZwischenSpeicher^.next;
+        if Sourcelist = nil then
+          begin
+            writeln('Sourcelist hatte keine Elemente mehr');
+          end
+        else
+        begin
+          writeln('Sourcelist ',i,' : ',SourceList^.info);
+        end;
+        
+        if i = 1 then {Beim ersten Element nur in neue Liste kopieren }
+          begin
+            DestinationList := ZwischenSpeicher;
+            DestinationList^.next := nil;
+            writeln('Erstes Elementwurde geschrieben');
+            GibListeAus1(DestinationList);
+            writeln('Listenausgaben nach einem Durchlauf');
+          end;
+        
+        if i = 2 then {Zweiter Durchgang muss nur einfach verglichen werden}
+          begin {Fügt Element nach dem erstem Element ein}
+            if DestinationList^.info <= ZwischenSpeicher^.info then
+              begin
+                DestinationList^.next := ZwischenSpeicher;
+                ZwischenSpeicher^.next := nil;
+              end
+            else  {Fügt Element vor dem erstem Element ein}
+            begin
+              ZwischenSpeicher^.next := DestinationList;
+              DestinationList := ZwischenSpeicher;
+              ZwischenSpeicher := nil;
+              ZwischenSpeicher^.next := nil;
+            end;
+            writeln('Zweites Element');
+            GibListeAus1(DestinationList);
+            writeln('Listenausgabe nach zweitem Durchlauf');
+          end;
 
+        if i > 2 then {Ab dritten Durchlauf wird hier iteriert}
+          begin
+            {Zuweisung der Durchlaufvariablen zur Einfügefunktion
+            DestAktList -> Aktuelles Element
+            PreDestList -> Element vor dem aktuellem Element}
+            DestAktList := DestinationList^.next;
+            PreDestList := DestinationList;
+            while DestAktList <> nil do
+              begin
+                writeln('Vergleiche: ',PreDestList^.info,' mit ', DestAktList^.info,' und ', ZwischenSpeicher^.info);
+                if (PreDestList^.info <= ZwischenSpeicher^.info) and (DestAktList^.info >= ZwischenSpeicher^.info) then {Wenn erfüllt, dann einfügen des aktuellen Elements}
+                  begin
+                   PreDestList^.next := ZwischenSpeicher;
+                   ZwischenSpeicher^.next := DestAktList; 
+                   writeln('Fuege ',ZwischenSpeicher^.info,' zwischen ',PreDestList^.info,' und ',DestAktList^.info,' ein');
+                  end;
 
+                if  ZwischenSpeicher^.info < DestinationList^.info then {Wenn Element kleiner als Listenanfang ist, am Anfang einfügen}
+                  begin
+                    ZwischenSpeicher^.next := DestinationList;
+                    DestinationList := ZwischenSpeicher;
+                  end;
+                
+                if (ZwischenSpeicher^.info > DestAktList^.info) and (DestAktList^.next = nil) then
+                  begin {Fügt letztes Listenelement ein}
+                    writeln('letztes Element wird eingefügt');
+                    DestAktList^.next := ZwischenSpeicher;
+                    ZwischenSpeicher := nil;
+                  end;
 
+                {Zuweisung des Nächsten Elements für den Durchlauf}
+                PreDestList := DestAktList;
+                DestAktList := PreDestList^.next;
+                GibListeAus1(DestinationList);
 
-
-
-
-
-
-
+              end;
+          end;
+        writeln('i: ', i);
+      end;
+      writeln('i: ', i);
+    ioRefListe := DestinationList;
+  end;
 
   procedure Anhaengen(var ioListe : tRefListe;
                         inZahl : tNatZahl);
