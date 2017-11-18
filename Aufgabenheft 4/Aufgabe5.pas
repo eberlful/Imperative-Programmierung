c68fe7de1b056ca8b82e94e6ef4e1e686b9d7022
eberlful <markusbenedikteberl@googlemail.com>
2017-11-18 09:51:59 +0100

    ...

--- Aufgabenheft 4/Aufgabe5.pas
+++ Aufgabenheft 4/Aufgabe5.pas
@@ -26,6 +26,7 @@ Welche der fünf folgenden Varianten geben die Knotenwerte für beliebige Binär
 }
 var
 auswahl : char;
+refBaumKnoten, refBaumBlatt1, refBaumBlatt2, refBaumBlatt3, refBaumBlatt4 : tRefBinBaum;
 
 procedure SymAusgabeA (
               inRefWurzel : tRefBinBaum);
@@ -36,9 +37,9 @@ begin
   begin
      write (inRefWurzel^.info);
      if inRefWurzel^.links <> nil  then
-       SymAusgabe(inRefWurzel^.links);
+       SymAusgabeA(inRefWurzel^.links);
      if inRefWurzel^.rechts <> nil then
-       SymAusgabe(inRefWurzel^.rechts)
+       SymAusgabeA(inRefWurzel^.rechts)
   end
 end; {SymAusgabe}
 
@@ -54,10 +55,10 @@ procedure SymAusgabeB (
  symmetrischer Reihenfolge aus}
 begin
   if inRefWurzel^.links <> nil  then
-    SymAusgabe(inRefWurzel^.links);
+    SymAusgabeB(inRefWurzel^.links);
   write(inRefWurzel^.info);
   if inRefWurzel^.rechts <> nil then
-    SymAusgabe(inRefWurzel^.rechts)
+    SymAusgabeB(inRefWurzel^.rechts)
 end; {SymAusgabe}
 
 
@@ -73,9 +74,9 @@ procedure SymAusgabeC (
 begin
   if inRefWurzel <> nil then
   begin
-    SymAusgabe(inRefWurzel^.links);
+    SymAusgabeC(inRefWurzel^.links);
     write (inRefWurzel^.info);
-    SymAusgabe(inRefWurzel^.rechts)
+    SymAusgabeC(inRefWurzel^.rechts)
   end
 end; {SymAusgabe}
 
@@ -94,9 +95,9 @@ begin
   if inRefWurzel <> nil then
   begin
     if inRefWurzel^.links <> nil  then
-      SymAusgabe(inRefWurzel^.links);
+      SymAusgabeD(inRefWurzel^.links);
     if inRefWurzel^.rechts <> nil then
-      SymAusgabe(inRefWurzel^.rechts);
+      SymAusgabeD(inRefWurzel^.rechts);
   write (inRefWurzel^.info)
   end
 end; {SymAusgabe}
@@ -108,7 +109,7 @@ end; {SymAusgabe}
 }
 
 
-procedure SymAusgabeD (
+procedure SymAusgabeE (
               inRefWurzel : tRefBinBaum);
 {gibt die Knotenwerte eines Binärbaums in
  symmetrischer Reihenfolge aus}
@@ -116,11 +117,11 @@ begin
   if inRefWurzel <> nil then
   begin
     if inRefWurzel^.links <> nil  then
-      SymAusgabe(inRefWurzel^.links);
+      SymAusgabeE(inRefWurzel^.links);
 
     write (inRefWurzel^.info);
     if inRefWurzel^.rechts <> nil then
-      SymAusgabe(inRefWurzel^.rechts)
+      SymAusgabeE(inRefWurzel^.rechts)
   end
 end; {SymAusgabe}
 
@@ -128,4 +129,54 @@ begin
   writeln('Geben Sie eine Auswahl ein (A-E)');
   readln(auswahl);
   
-end;
+  new (refBaumKnoten);
+  new (refBaumBlatt1);
+  new (refBaumBlatt2);
+  new (refBaumBlatt3);
+  new (refBaumBlatt4);
+
+  refBaumKnoten^.info := 6;
+  refBaumKnoten^.rechts := refBaumBlatt2;
+  refBaumKnoten^.links := refBaumBlatt1;
+
+  refBaumBlatt1^.info := 3;
+  refBaumBlatt1^.rechts := refBaumBlatt3;
+  refBaumBlatt1^.links := refBaumBlatt4;
+
+  refBaumBlatt2^.info := 8;
+  refBaumBlatt2^.rechts := nil;
+  refBaumBlatt2^.links := nil;
+
+  refBaumBlatt3^.info := 1;
+  refBaumBlatt3^.rechts := nil;
+  refBaumBlatt3^.links := nil;
+
+  refBaumBlatt4^.info := 4;
+  refBaumBlatt4^.rechts := nil;
+  refBaumBlatt4^.links := nil;
+
+  if (auswahl = 'A') or (auswahl = 'a') then
+  begin
+    SymAusgabeA(refBaumKnoten);
+  end;
+
+  if (auswahl = 'B') or (auswahl = 'b') then
+  begin
+    SymAusgabeB(refBaumKnoten);
+  end;
+
+  if (auswahl = 'C') or (auswahl = 'c') then
+  begin
+    SymAusgabeC(refBaumKnoten);
+  end;
+
+  if (auswahl = 'D') or (auswahl = 'd') then
+  begin
+    SymAusgabeD(refBaumKnoten);
+  end;
+
+  if (auswahl = 'E') or (auswahl = 'e') then
+  begin
+    SymAusgabeE(refBaumKnoten);
+  end;  
+end.
